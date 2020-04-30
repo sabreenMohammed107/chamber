65db31f22b85d12ee0daa6772e1315401b61e9db
sabreenMohammed107 <sabreenm312@gmail.com>
2020-04-30 16:43:12 +0200

    fg

--- resources/views/Customer/home/index.blade.php
+++ resources/views/Customer/home/index.blade.php
@@ -204,7 +204,7 @@
                           box-shadow: 2px 2px 2px 2px #2a282852;
                           text-align: center;
                           margin-bottom: 16px;
-                          position: relative;
+                         
                           background-color: #fff;
                           background-clip: border-box;
                           border: 0;
@@ -222,18 +222,18 @@
   flex-wrap: wrap;">
                         @foreach($news as $new)
 
-                        <div style="flex: 1 0 31%; 
+                        <div style="flex: 1 1 30%; 
   margin: 5px;
 ">
                           <div class="cardx">
                             @if($new->gallery!=null && $new->gallery->first() !=null && $new->gallery->first()->order==1)
                             @if($new->gallery->first()->image!=null)
-                            <img style="width: 100% ;height:200px" src="{{ asset('uploads/news/'.$new->gallery->first()->image) }}" alt="...">
+                            <img style="width:100%" src="{{ asset('uploads/news/'.$new->gallery->first()->image) }}" alt="...">
                             @else
                             <iframe id="popup-youtube-player" width="100%" height="200" src="{{$new->gallery->first()->vedio}}" frameborder="0" allowfullscreen="true" allowscriptaccess="always"></iframe>
                             @endif
                             @else
-                            <img src="" style="width:100%;height:200px" alt="no image">
+                            <img src=""  alt="no image">
                             @endif
                             <div class="card-body">
                               <h5>
