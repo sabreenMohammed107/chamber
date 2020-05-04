20d68fa4c0e1ec8e6d46b6395afe4ad5307ccbc0
sabreenMohammed107 <sabreenm312@gmail.com>
2020-05-04 13:18:39 +0200

    cc

--- resources/views/Customer/news/newsDetailsList.blade.php
+++ resources/views/Customer/news/newsDetailsList.blade.php
@@ -113,6 +113,13 @@
             <div class="card-body">
 
               <a href="{{ url('newsDetails/'.$gallerynew->relatednews->id) }}">
+              <h5>
+            @if(app()->getLocale()=='en')
+            {{$gallerynew->relatednews->en_title}}
+            @else
+            {{$gallerynew->relatednews->ar_title}}
+            @endif
+          </h5>
                 <p class="card-text">
 
 
