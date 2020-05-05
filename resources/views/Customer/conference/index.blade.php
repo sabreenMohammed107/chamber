2eba61a53baf6e101524d3af9006c360fe1ed5d6
sabreenMohammed107 <sabreenm312@gmail.com>
2020-05-05 22:44:37 +0200

    bbbbb

--- resources/views/Customer/conference/index.blade.php
+++ resources/views/Customer/conference/index.blade.php
@@ -23,7 +23,7 @@
                 <div class="col-md-3">
                     <div class="sidebar2 " style="box-shadow: 1px 1px 1px 1px #EEE;">
                         <a class="active addArow" href="#advertes-dept" onclick="advertesdept()">{{ __('titles.events') }}  </a>
-                        <a  href="#cal-dept" onclick="caldept()">{{ __('titles.calen') }} </a>
+                        <!-- <a  href="#cal-dept" onclick="caldept()">{{ __('titles.calen') }} </a> -->
                        
                        
                        
